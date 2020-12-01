9f71e961e1bde73931cf826aaf2658360d36fc48
TheDudeFromCI <thedudefromci@gmail.com>
2020-12-01 05:57:21 -0700

    Created Java project

---
+++ src/main/java/net/wraithavengames/sarica/goalsolver/App.java
@@ -0,0 +1,14 @@
+package net.wraithavengames.sarica.goalsolver;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static final Logger log = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        log.info("Starting Emotional Database...");
+    }
+}
